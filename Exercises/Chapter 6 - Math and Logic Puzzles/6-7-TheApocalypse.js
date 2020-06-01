8b4efbf77575f202ec124bcf8fb086436ce9e6ec
lucas-aguilar <lucas.a23@gmail.com>
2020-06-01 17:17:57 -0300

    Qst 6-7

---
+++ Exercises/Chapter 6 - Math and Logic Puzzles/6-7-TheApocalypse.js
@@ -0,0 +1,45 @@
+function simulateRegularBirth(
+  families = DEFAULT_FAMILIES,
+  avgPerFamily = DEFAULT_AVG_KIDS
+) {
+  let boys = 0;
+  let girls = 0;
+  let kids;
+  for (let qty = 0; qty < families; qty++) {
+    kids = 0;
+    while (kids < avgPerFamily) {
+      if (getRandomBool()) boys++;
+      else girls++;
+      kids++;
+    }
+  }
+  return {
+    boys: boys,
+    girls: girls,
+  };
+}
+
+function simulateBirthPolicy(families = DEFAULT_FAMILIES) {
+  let boys = 0;
+  let girls = 0;
+  for (let qty = 0; qty < families; qty++) {
+    while (getRandomBool()) boys++;
+    girls++;
+  }
+  return {
+    boys: boys,
+    girls: girls,
+  };
+}
+
+function getRandomBool() {
+  return Math.random() >= 0.3;
+}
+
+var DEFAULT_FAMILIES = 10000;
+var DEFAULT_AVG_KIDS = 2;
+
+console.log('BirthPolicy:');
+console.log(simulateBirthPolicy());
+console.log('RegularBirth:');
+console.log(simulateRegularBirth());
